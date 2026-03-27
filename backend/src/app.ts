46dd128e3b868b17a6a99e0fd4d52af8104e5d1f
Uttam Pun <uttampun50@gmail.com>
2026-03-27 22:13:10 +0000

    chore: init project structure

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express, {type Express, type Request, type Response} from "express";
+import chalk from "chalk";
+
+const app: Express = express();
+
+const Port = process.env.PORT || 8080;
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello from the backend!" });
+});
+
+app.listen(Port, () => {
+  console.log(chalk.green(`Server is running at http://localhost:${Port}`));
+});
