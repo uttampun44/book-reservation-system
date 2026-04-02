fc201a2e02bbf1ad34a9109e323211c0ce1016be
Uttam Pun <uttampun50@gmail.com>
2026-04-03 00:17:56 +0100

    fix: server.ts remove

--- backend/src/server.ts
+++
@@ -1,7 +0,0 @@
-import chalk from "chalk";
-import { startServer } from "./app";
-
-startServer().catch(err => {
-  console.error(chalk.red("Failed to start server:"), err);
-  process.exit(1);
-});
