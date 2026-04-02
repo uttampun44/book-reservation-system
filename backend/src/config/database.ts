d63c0d3b99720837dc9616a4d5d191302688e84e
Uttam Pun <uttampun50@gmail.com>
2026-04-03 00:42:11 +0100

    fix: mongodb issue

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,9 +2,12 @@ import mongoose from "mongoose";
 import { configEnv } from "@config/env";
 import Chalk from 'chalk'
 
-const connectDB =  async () => {
+const connectDB = async () => {
   try {
-    await mongoose.connect(configEnv.mongoURI);
+    await mongoose.connect(configEnv.mongoURI, {
+      connectTimeoutMS: 10000,
+      serverSelectionTimeoutMS: 10000,
+    });
     console.log(Chalk.yellow("Database connected successfully"));
   } catch (error) {
     console.error(Chalk.red("Database connection error:", error));
