5065064c93c1d0fcb5bc26f56d1363a80ce23619
Uttam Pun <uttampun50@gmail.com>
2026-04-03 01:20:25 +0100

    fix: load env variables for MongoDB connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,22 @@
-// import dotenv from "dotenv";;
-// dotenv.config();
+
 import express, { type Express } from "express";
+
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
 import chalk from "chalk";
 import Routes from "@routes/Routes";
 import { corsOptions } from "@config/cors";
 import Cors from "cors";
 import connectDB from "@config/database";
 
-
+console.log("MONGO URI:", process.env.VERCEL_MONGO_URI);
 export const app: Express = express();
 
 const Port = process.env.PORT || 8080;
