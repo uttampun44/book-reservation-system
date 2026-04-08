2c8de5ca0f1ee6ee6b37a668c07cba18e644e717
Uttam Pun <uttampun50@gmail.com>
2026-04-08 13:24:18 +0100

    fix: backend api url

--- frontend/src/features/books/api/getBookList.ts
+++ frontend/src/features/books/api/getBookList.ts
@@ -4,13 +4,12 @@ import type { BookListResponse } from "../types/book";
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:8000",
   timeout: 10000,
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-console.log("API URL:", import.meta.env.VITE_BASE_URL);
+console.log("API URL:", import.meta.env.VITE_BACKEND_URL);
 
 export const getBooks = async (
   page: number = 1,
